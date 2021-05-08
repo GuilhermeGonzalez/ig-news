4d67985a0bf29c56cc67e2ff84dadf7d601d2224
Guilherme C Gonzalez <gcgonzalez99@hotmail.com>
2021-05-08 19:24:32 -0300

    feat: user info saved at faunadb

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -9,13 +9,21 @@ export function SignInButton() {
     const [session] = useSession()
 
     return session ? (
-        <button className={styles.signInButton} onClick={() => signOut()} type="button">
+        <button
+            className={styles.signInButton}
+            onClick={() => signOut()}
+            type="button"
+        >
             <FaGithub color="#04d361" />
             {session.user.name}
             <FiX color="#737380" className={styles.closeIcon} />
         </button>
     ) : (
-        <button className={styles.signInButton} onClick={() => signIn('github')} type="button">
+        <button
+            className={styles.signInButton}
+            onClick={() => signIn('github')}
+            type="button"
+        >
             <FaGithub color="#eba417" />
             Sign in with Github
         </button>
