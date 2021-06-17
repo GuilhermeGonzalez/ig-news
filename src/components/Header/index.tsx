93886f417f3e9b5ec030cbd2a45eedc5f8111f2b
Guilherme C Gonzalez <gcgonzalez99@hotmail.com>
2021-06-16 21:27:28 -0300

    feat: controle de acessi oeka sessão do usuario logado

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,20 +1,21 @@
 import { SignInButton } from '../SignInButton'
 import styles from './styles.module.scss'
-import Link from 'next/link'
-
+import { ActiveLink } from '../ActiveLink';
 
 export function Header() {
+
+
     return (
         <header className={styles.headerContainer}>
             <div className={styles.headerContent}>
                 <img src="/images/logo.svg" alt="logo ignews" />
                 <nav>
-                    <Link href="/">
-                        <a className={styles.active}>Home</a>
-                    </Link>
-                    <Link href="/posts" prefetch>
-                        <a >Posts</a>
-                    </Link>
+                    <ActiveLink activeClassName={styles.active} href="/">
+                        <a>Home</a>
+                    </ActiveLink>
+                    <ActiveLink activeClassName={styles.active} href="/posts">
+                        <a> Posts</a>
+                    </ActiveLink>
                 </nav>
 
                 <SignInButton />
